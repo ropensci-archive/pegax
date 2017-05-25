d8bea8ffda7f595b14141c1913b6b8ed5be113ef
Scott Chamberlain <myrmecocystus@gmail.com>
2017-05-25 14:49:17 -0700

    first stuff :bluasfasfdafsd:

---
+++ R/pegax-package.R
@@ -0,0 +1,8 @@
+#' PEG for taxonomy
+#' 
+#' @useDynLib Rcpp
+#' @name pegax-package
+#' @aliases pegax
+#' @docType package
+#' @keywords package
+NULL
---
+++ src/peg.cpp
@@ -0,0 +1,5 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::depends(piton)]]
+#include <pegtl.hpp>
